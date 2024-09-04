2ce83ab96cfb38950523f1938932fb235e349a0f
svs24 <sangeethasukumaran24@gmail.com>
2024-09-04 14:40:04 -0400

    Committed first bash file

---
+++ time.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script reads and displays the current time.
+
+# Get the current time in a human-readable format
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Print the current time
+echo "Current time: $current_time"
+
